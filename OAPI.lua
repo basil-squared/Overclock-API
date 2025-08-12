c4bc1fc4f4ff011dfdae3e2f1fd0c446aa5948bc
Juniper <basil.squared1@gmail.com>
2025-08-12 18:43:02 -0500

    putting in some needed files, time to get to work

---
+++ OAPI.lua
@@ -0,0 +1,14 @@
+OAPI = {}
+OAPI.F = {}
+OAPI.G = {}
+OAPI.G.MP = SMODS.current_mod_path
+local NFS = require('nativefs')
+
+SMODS.Rarity {
+    key = 'overclocked',
+    loc_txt = {
+        name = "Overclocked"
+    },
+    badge_colour = HEX("00F0FF"),
+    pools = {}
+}
